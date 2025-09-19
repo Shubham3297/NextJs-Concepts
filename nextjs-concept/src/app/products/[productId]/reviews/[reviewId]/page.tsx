b772e85ae4c97dfc2cf0a8552aa89f13e26e1393
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2025-09-19 21:22:06 +0530

    Page Not Found

--- nextjs-concept/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ nextjs-concept/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,16 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductReviews({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 100) {
+    notFound();
+  }
+
   return (
     <h1>
       Review {reviewId} for Product {productId}
