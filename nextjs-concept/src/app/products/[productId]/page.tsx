d56d432c8cabd81e6b9ceaef8ec550fc00b92166
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2026-07-28 21:37:40 +0530

    Dynamic Routing Metadata

--- nextjs-concept/src/app/products/[productId]/page.tsx
+++ nextjs-concept/src/app/products/[productId]/page.tsx
@@ -1,8 +1,29 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: String }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const Id = (await params).productId;
+  return {
+    title: `Product ${Id}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Product Details - {productId}</h1>;
 }
+
+// Old - 1
+// export default async function ProductDetails({
+//   params,
+// }: {
+//   params: Promise<{ productId: String }>;
+// }) {
+//   const productId = (await params).productId;
+//   return <h1>Product Details - {productId}</h1>;
+// }
