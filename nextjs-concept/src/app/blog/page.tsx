2c813a2b08ccf1ca7dfb9d5d627e706f7da98675
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2026-07-30 22:21:44 +0530

    Title Metadata - Absolute

--- nextjs-concept/src/app/blog/page.tsx
+++ nextjs-concept/src/app/blog/page.tsx
@@ -1,8 +1,14 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blog",
+  title: {
+    absolute: "Blog",
+  },
 };
+
+// export const metadata: Metadata = {
+//   title: "Blog",
+// };
 
 export default function page() {
   return <h1>Blog</h1>;
