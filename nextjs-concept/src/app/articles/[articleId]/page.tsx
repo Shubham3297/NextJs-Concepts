660f19e3709dccb96dfce86bd5b9c17b9473ceef
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2026-08-06 21:57:01 +0530

    params and searchParams in Client component

--- nextjs-concept/src/app/articles/[articleId]/page.tsx
+++ nextjs-concept/src/app/articles/[articleId]/page.tsx
@@ -1,14 +1,43 @@
-import Link from "next/link";
+// For Server component
+// import Link from "next/link";
 
-export default async function NewsArticle({
+// export default async function NewsArticle({
+//   params,
+//   searchParams,
+// }: {
+//   params: Promise<{ articleId: String }>;
+//   searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
+// }) {
+//   const { articleId } = await params;
+//   const { lang = "en" } = await searchParams;
+
+//   return (
+//     <>
+//       <div>News article {articleId}</div>
+//       <div>Read in {lang}</div>
+//       <br></br>
+//       <div>
+//         <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+//         <Link href={`/articles/${articleId}?lang=es`}>Spanish</Link>
+//         <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
+//       </div>
+//     </>
+//   );
+// }
+
+"use client";
+import Link from "next/link";
+import { use } from "react";
+
+export default function NewsArticle({
   params,
   searchParams,
 }: {
   params: Promise<{ articleId: String }>;
   searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
 }) {
-  const { articleId } = await params;
-  const { lang = "en" } = await searchParams;
+  const { articleId } = use(params);
+  const { lang = "en" } = use(searchParams);
 
   return (
     <>
