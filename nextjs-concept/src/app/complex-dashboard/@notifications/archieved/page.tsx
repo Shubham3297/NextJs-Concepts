770abffe18de1c81c462c2afa781dccbacb76db1
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2026-09-13 12:01:03 +0530

    31 - Unmatched Routes

---
+++ nextjs-concept/src/app/complex-dashboard/@notifications/archieved/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@/components/card";
+import Link from "next/link";
+
+export default function ArchievedNotifications() {
+  return (
+    <Card>
+      <div>Archieved Notifications</div>
+      <div>
+        <Link href="/complex-dashboard">Default</Link>
+      </div>
+    </Card>
+  );
+}
--- nextjs-concept/src/app/complex-dashboard/@notifications/page.tsx
+++ nextjs-concept/src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,13 @@
 import { Card } from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link href="/complex-dashboard/archieved">Archieved</Link>
+      </div>
+    </Card>
+  );
 }
